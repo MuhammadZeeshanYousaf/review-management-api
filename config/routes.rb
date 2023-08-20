c55a0795b8d4bd9a0af7c3e7f709b389a0367ecc
Muhammad Zeeshan Yousaf <zeeshanyousaf430@gmail.com>
2023-08-20 20:57:05 +0500

    Scope the routes with api

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,19 @@
 Rails.application.routes.draw do
 
-  devise_for :users, path: '', path_names: {
-    sign_in: 'login',
-    sign_out: 'logout',
-    registration: 'signup'
-  },
-  controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations'
-  }
+  scope 'api' do
+    devise_for :users, path: '', path_names: {
+      sign_in: 'login',
+      sign_out: 'logout',
+      registration: 'signup'
+    },
+    controllers: {
+      sessions: 'users/sessions',
+      registrations: 'users/registrations'
+    }
 
-  resource :business do
-    resources :reviews
+    resource :business do
+      resources :reviews
+    end
   end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
